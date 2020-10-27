f1f874922a0b70bbe50e59718f2c6b06a352df5d
Danilo Alves <hardwayinfo@gmail.com>
2020-10-27 16:11:46 +0000

    Menu implemantation Switch for exit implemanted Shows all vehicles.

--- main.c
+++ main.c
@@ -6,59 +6,104 @@ typedef struct {
 char marca[10];
 char modelo [10];
 char ano [10];
+char placa [10];
 } veiculos;
 
 veiculos vei [10];
 
 void cadastroDeVeiculos(){
-int i = 0;
-for (i = 0; i < 3; i++){
-printf("Insira a marca:");
-gets(vei[i].marca);
-printf("Insira o modelo:");
-gets(vei[i].modelo);
-printf("Insira o ano:");
-gets(vei[i].ano);
-}
+  int i = 0;
+  for (i = 0; i < 3; i++){
+  printf("Insira a marca:");
+  scanf("%s", vei[i].marca);
+  printf("Insira o modelo:");
+  scanf("%s", vei[i].modelo);
+  printf("Insira o ano:");
+  scanf("%s", vei[i].ano);
+  printf("Insira a placa:");
+  scanf("%s", vei[i].placa);
+  }
 }
 
 void buscaAno(){
-int i = 0;
-int cont = 1;
-char ano[4];
-printf("Insira o ano para busca:");
-scanf("%s", ano);
-for (i = 0; i < 3; i++){
-if (!strcmp(vei[i].ano, ano)){
-printf("%s%s%s", vei[i].ano, vei[i].marca, vei[i]. modelo);
-}else
-printf("Não há veículo para a busca");
-}
+  int i = 0;
+  int cont = 1;
+  char ano[4];
+  printf("Insira o ano para busca:");
+  scanf("%s", ano);
+  for (i = 0; i < 3; i++){
+  if (!strcmp(vei[i].ano, ano)){
+  printf("%s%s%s", vei[i].ano, vei[i].marca, vei[i]. modelo);
+  }else
+  printf("Não há veículo para a busca");
+  }
 }
 
 int main(void) {
 int i = 0;
-char c;
+int option;
+
+loop:
 
 printf("========================================================\n");
 printf("Insira a opção desejada:");
-c = getchar();
+printf("\n[1] - Para listar todos os veículos.");
+printf("\n[2] - Para cadastrar um novo veículo.");
+printf("\n[3] - Para pesquisar os veículos por modelo.");
+printf("\n[4] - Para pesquisar os veículos por ano.");
+printf("\n[5] - Para sair;");
+printf("\n");
 
-switch (c){
+scanf("%d", &option);
 
-case "l" :
-    printf("E segunda-feira.");
+  switch (option){
+
+    case 1 :
+    
+    printf("%s\n%s\n%s\n%s", vei[i].marca, vei[i].modelo, vei[i].ano, vei[i].placa);
+    
+    getchar();
+
     break;
 
+    case 2 :
 
+    cadastroDeVeiculos();
+    for (i=0; i < 3; i++)
 
+    break;
+
+    case 3 :
+
+    //buscar por modelo
+
+    break;
+
+    case 4 :
+
+    buscaAno();
+
+    break;
+
+    case 5 :
+    
+    goto sair;
+
+    break;
+
+    default:
+
+    printf("Opção inválida.");
+
+    break;
+
+  }
+  
+  goto loop;
+  sair:;
+  printf("Programa encerrado.");
+  getchar();
 }
 
-cadastroDeVeiculos();
-for (i=0; i < 3; i++){
-printf("%s\n%s\n", vei[i].marca, vei[i].modelo);
-}
-buscaAno();
-getchar();
-}
+
 
